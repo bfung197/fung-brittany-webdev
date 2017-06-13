6a273448025c2f5cd08a1d6862a13a1c3b4d3d23
bfung197 <fung.b@husky.neu.edu>
2017-06-13 14:17:06 -0400

    upload image works

--- public/assignment/views/widget/controllers/widget-choose.controller.client.js
+++ public/assignment/views/widget/controllers/widget-choose.controller.client.js
@@ -12,7 +12,6 @@
 
         // event handlers
         model.createWidget = createWidget;
-        model.widgetType = widgetType;
 
         function init() {
             widgetService
@@ -24,14 +23,27 @@
 
         init();
 
-        // implementation
-        function widgetType(type) {
-            model.type = type;
-        }
+        function createWidget(widgetType) {
+            switch (widgetType) {
+                case "HEADING":
+                    widget =  {'name': '', 'type': widgetType, '_page': model.pageId, 'size': '', 'text': '', 'order': 1000};
+                    break;
+                case "IMAGE":
+                    widget =  {'name': '', 'type': widgetType, '_page': model.pageId, 'width': '', 'url': '', 'text': '', 'order': 1000};
+                    break;
+                case "YOUTUBE":
+                    widget =  {'name': '', 'type': widgetType, '_page': model.pageId, 'width': '', 'url': '', 'text': '', 'order': 1000};
+                    break;
+                case "TEXT":
+                    widget =  {'name': '', 'type': widgetType, '_page': model.pageId, 'rows': '', 'placeholder': '', 'formatted': '', 'order': 1000};
+                    break;
+                case "HTML":
+                    widget =  {'name': '', 'type': widgetType, '_page': model.pageId, 'text': '', 'order': 1000};
+                    break;
+                default:
+                    break;
+            }
 
-        function createWidget(widget) {
-            var widget = {};
-            widget.type = model.type;
             widgetService
                 .createWidget(model.pageId, widget)
                 .then(function(widget) {
